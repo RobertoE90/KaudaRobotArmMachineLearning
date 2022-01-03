344531e9d08a97c01489b7d435f42945978e4e05
Roberto Estrada <robertoestradaleyva@gmail.com>
2022-01-03 14:27:59 -0400

    Implement rotation by normalized value

--- Assets/Scripts/NemaMotorController.cs
+++ Assets/Scripts/NemaMotorController.cs
@@ -12,29 +12,42 @@ public class NemaMotorController : MonoBehaviour
     [SerializeField] private float _maxAngle;
 
     [SerializeField] private bool _debug = false; 
-    private float _initialRotationAxisValue;
-    private float _targetValue;
-    
+    private float _currentNormalizedRotationValue;
+    private float _targetNormalizedRotationValue;
+    private float _rotationStep;
+
+    private void Awake()
+    {
+        _currentNormalizedRotationValue = 0;
+        _rotationStep = _rotationSpeed / (_maxAngle - _minAngle);
+        SetJointRotation(_currentNormalizedRotationValue);
+    }
 
     public void SetTargetValue(float value)
     {
-        var currentLocalRotationEuler = transform.localRotation.eulerAngles;
-        var inAxisVector = Vector3.Scale(currentLocalRotationEuler, GetRotationAxis());
-        _initialRotationAxisValue = inAxisVector.magnitude;
-
-        _targetValue = Mathf.Lerp(_minAngle, _maxAngle, value);
-        _targetValue = _targetValue % 360f;
-        _targetValue = Mathf.Clamp(_targetValue, _minAngle, _maxAngle);
+        _targetNormalizedRotationValue = value;
     }
 
     public void Tick()
     {
-        var lerpedRotationValue = Mathf.Lerp(_initialRotationAxisValue, _targetValue, _rotationSpeed * Time.deltaTime * 0.1f);
+        var incrementSign = (_currentNormalizedRotationValue < _targetNormalizedRotationValue) ? 1 : -1;
 
-        if(_debug)
-            Debug.Log($"init {_initialRotationAxisValue} target{_targetValue} lerped{lerpedRotationValue}");
+        if(Mathf.Abs(_currentNormalizedRotationValue - _targetNormalizedRotationValue) > _rotationStep * Time.deltaTime)
+        {
+            _currentNormalizedRotationValue += (incrementSign) * _rotationStep * Time.deltaTime;
+        }
+        else
+        {
+            _currentNormalizedRotationValue = _targetNormalizedRotationValue;
+        }
 
-        transform.localRotation = Quaternion.Euler(lerpedRotationValue * GetRotationAxis());
+        SetJointRotation(_currentNormalizedRotationValue);
+    }
+
+    private void SetJointRotation(float normalizedRotation)
+    {
+        var rotationValue = Mathf.Lerp(_minAngle, _maxAngle, normalizedRotation) * GetRotationAxis();
+        transform.localRotation = Quaternion.Euler(rotationValue);
     }
 
     private Vector3 GetRotationAxis()
